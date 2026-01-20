4ba7260b12f2b56b4d013b0d888d645fe92ea5e8
quoczai <kienquoctrinhfacebook@gmail.com>
2026-01-20 16:38:32 +0700

    btvn buoi 2

---
+++ 2001240399_Trinh_Huu_Kien_Quoc_Buoi2/Bai4.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2001240399_Trinh_Huu_Kien_Quoc_Buoi2
+{
+    /// <summary>
+    /// Interaction logic for Bai4.xaml
+    /// </summary>
+    public partial class Bai4 : Window
+    {
+        public Bai4()
+        {
+            InitializeComponent();
+        }
+    }
+}
