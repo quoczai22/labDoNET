9640bd413631ceb9880e90bf3617223136fa21d7
quoczai22 <kienquoctrinhfacebook@gmail.com>
2026-01-21 01:35:31 +0700

    hoan thanh bt va  dotNET

--- 2001240399_Trinh_Huu_Kien_Quoc_Buoi2/Bai2.xaml.cs
+++ 2001240399_Trinh_Huu_Kien_Quoc_Buoi2/Bai2.xaml.cs
@@ -23,5 +23,22 @@ namespace _2001240399_Trinh_Huu_Kien_Quoc_Buoi2
         {
             InitializeComponent();
         }
+        public void ChangeTheme(string themeName)
+        {
+            var dict=new ResourceDictionary {Source =new Uri($"Themes/{themeName}.xaml",UriKind.Relative)};
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboX.SelectedIndex == 0)
+            {
+                ChangeTheme("ThemeLight");
+            }
+            else{
+                ChangeTheme("ThemeDark");
+            }
+        }
     }
 }
--- 2001240399_Trinh_Huu_Kien_Quoc_Buoi2/Bai4.xaml.cs
+++ 2001240399_Trinh_Huu_Kien_Quoc_Buoi2/Bai4.xaml.cs
@@ -23,5 +23,99 @@ namespace _2001240399_Trinh_Huu_Kien_Quoc_Buoi2
         {
             InitializeComponent();
         }
+
+        private void LoaiPT(object sender, RoutedEventArgs e)
+        {
+            if (bac1.IsChecked == true)
+            {
+                a.Visibility = Visibility.Visible;
+                bien_a.Visibility = Visibility.Visible;
+                b.Visibility = Visibility.Visible;
+                bien_b.Visibility = Visibility.Visible;
+                c.Visibility = Visibility.Hidden;
+                bien_c.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                a.Visibility = Visibility.Visible;
+                bien_a.Visibility = Visibility.Visible;
+                b.Visibility = Visibility.Visible;
+                bien_b.Visibility = Visibility.Visible;
+                c.Visibility = Visibility.Visible;
+                bien_c.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void GiaiPT(object sender, RoutedEventArgs e)
+        {
+            double a,b, c;
+            if (!double.TryParse(bien_a.Text, out  a) ||
+               !double.TryParse(bien_b.Text, out  b))
+            {
+                showkq.Text = ("Vui lòng nhập số hợp lệ!");
+                return;
+            }
+            if (bac2.IsChecked == true)
+            {
+                if (!double.TryParse(bien_c.Text, out c))
+                {
+                    showkq.Text = ("Vui lòng nhập số hợp lệ!");
+                    return;
+                }
+                if (a == 0)
+                {
+                    showkq.Text = ("Hệ số 'a' phải khác 0 trong phương trình bậc hai!");
+                    return;
+                }
+                double delta = b * b - 4 * a * c;
+                if (delta < 0)
+                {
+                    showkq.Text = ("Phương trình vô nghiệm!");
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    showkq.Text = ($"Phương trình có nghiệm kép x1 = x2 = {x}");
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    showkq.Text = ($"Phương trình có hai nghiệm phân biệt x1 = {x1} \n x2 = {x2}");
+                }
+            }
+            else
+            {
+                if (a == 0)
+                {
+                    if (b == 0)
+                    {
+                        showkq.Text = ("Phương trình vô số nghiệm!");
+                    }
+                    else
+                    {
+                        showkq.Text = ("Phương trình vô nghiệm!");
+                    }
+                }
+                else
+                {
+                    double x = -b / a;
+                    showkq.Text = ($"Phương trình có nghiệm x = {x}");
+                }
+            }
+        }
+
+        private void Thoat(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (bac1.IsChecked != true && bac2.IsChecked != true)
+            {
+                showkq.Text = ("Vui lòng chọn loại phương trình!");
+            }
+        }
     }
 }
