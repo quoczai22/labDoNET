ba3d7e32af38127f5b81aad9918e314df7c59562
quoczai22 <kienquoctrinhfacebook@gmail.com>
2026-04-14 16:36:57 +0700

    lam lab 10 nhung chua chuan

---
+++ 2001240399_Trinh_Huu_Kien_Quoc_Buoi10/ViewModels/KhoaViewModel.cs
@@ -0,0 +1,120 @@
+﻿using _2001240399_Trinh_Huu_Kien_Quoc_Buoi10.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace _2001240399_Trinh_Huu_Kien_Quoc_Buoi10.ViewModels
+{
+    public class KhoaViewModel : BaseViewModel
+    {
+        QLSinhVienEntities1 db = new QLSinhVienEntities1();
+
+        public ObservableCollection<Khoa> DS_Khoa { get; set; }
+        public RelayCommand AddCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+        public RelayCommand UpdateCommand { get; set; }
+
+        public Khoa NewKhoa { get; set; }
+
+        private Khoa _SelectedKhoa;
+        public Khoa SelectedKhoa
+        {
+            get => _SelectedKhoa;
+            set
+            {
+                _SelectedKhoa = value;
+                OnPropertyChanged(nameof(SelectedKhoa));
+
+                if (SelectedKhoa != null)
+                {
+                    NewKhoa = new Khoa()
+                    {
+                        MaKhoa = _SelectedKhoa.MaKhoa,
+                        TenKhoa = _SelectedKhoa.TenKhoa
+                    };
+                    OnPropertyChanged(nameof(NewKhoa));
+                }
+            }
+        }
+
+        public KhoaViewModel()
+        {
+            NewKhoa = new Khoa();
+
+            AddCommand = new RelayCommand(o => Add());
+            DeleteCommand = new RelayCommand(o => Delete(), o => SelectedKhoa != null);
+            UpdateCommand = new RelayCommand(o => Update(), o => SelectedKhoa != null);
+
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            DS_Khoa = new ObservableCollection<Khoa>(db.Khoas.ToList());
+            OnPropertyChanged(nameof(DS_Khoa));
+            SelectedKhoa = DS_Khoa.FirstOrDefault();
+        }
+
+        private void Add()
+        {
+            var newKhoa = new Khoa
+            {
+                MaKhoa = NewKhoa.MaKhoa,
+                TenKhoa = NewKhoa.TenKhoa
+            };
+
+            DS_Khoa.Add(newKhoa);
+            db.Khoas.Add(newKhoa);
+            SelectedKhoa = newKhoa;
+
+            db.SaveChanges();
+
+            NewKhoa.MaKhoa = "";
+            NewKhoa.TenKhoa = "";
+            OnPropertyChanged(nameof(NewKhoa));
+        }
+
+        private void Delete()
+        {
+            if (SelectedKhoa == null)
+                return;
+
+            if (MessageBox.Show("Bạn chắc chắn muốn xóa khoa này?",
+                                "Xác nhận",
+                                MessageBoxButton.YesNo,
+                                MessageBoxImage.Question) != MessageBoxResult.Yes)
+                return;
+
+            db.Khoas.Remove(SelectedKhoa);
+            DS_Khoa.Remove(SelectedKhoa);
+            db.SaveChanges();
+            SelectedKhoa = DS_Khoa.FirstOrDefault();
+        }
+
+        private void Update()
+        {
+            try
+            {
+                if (SelectedKhoa == null) return;
+
+                if (SelectedKhoa.MaKhoa != NewKhoa.MaKhoa)
+                {
+                    MessageBox.Show("Không được phép thay đổi Mã khoa (Khóa chính). Vui lòng chỉ cập nhật Tên khoa.");
+                    NewKhoa.MaKhoa = SelectedKhoa.MaKhoa;
+                    OnPropertyChanged(nameof(NewKhoa));
+                    return;
+                }
+                // GÁN NGƯỢC: NewKhoa -> SelectedKhoa
+                SelectedKhoa.MaKhoa = NewKhoa.MaKhoa;
+                SelectedKhoa.TenKhoa = NewKhoa.TenKhoa;
+                db.SaveChanges();
+                LoadData();
+                MessageBox.Show("Cập nhật thành công!");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Lỗi khi cập nhật: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ 2001240399_Trinh_Huu_Kien_Quoc_Buoi10/Views/QuanLyKhoaView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2001240399_Trinh_Huu_Kien_Quoc_Buoi10.Views
+{
+    /// <summary>
+    /// Interaction logic for QuanLyKhoaView.xaml
+    /// </summary>
+    public partial class QuanLyKhoaView : Window
+    {
+        public QuanLyKhoaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
